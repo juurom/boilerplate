7dd8cc386dff7217c3d918d3eaea22dc148b6b9a
juyeon <01bjy@kakao.com>
2022-07-06 17:43:38 +0900

    20220706 토큰 생성 (~12강)

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const express = require('express') //express 모듈 가져오기
 const app = express() //새로운 expree앱 만들기
 const port = 5000 //백서버 포트
 const bodyParser = require('body-parser'); //bodyparser가져오기
+const cookieParser = require('cookie-parser');//cookieparser, cookie에 저장할 때 사용
 const { User } = require("./models/User"); //models/user에서 user객체 가져오기
 //bodyparser: client에서 오는 정보를 server가 분석할 수 있게 해줌
 //20220706 어 근데 강의댓글보니까 업데이트돼서 안써도된대
 
 const config = require("./config/key");
-
 
 //application/x-www-form-urlencoded <= 이렇게 된 데이터를 분석해서 가져올 수 있게
 //app.use(bodyParser.urlencoded({extended: true}));
@@ -22,10 +22,9 @@ app.use(express.urlencoded({extended:true}))
 //json <=json 타입의 데이터
 app.use(bodyParser.json()); //<<bodyparser로 json받기... 
 //app.use(express.json())
+app.use(cookieParser());
 
 const mongoose = require('mongoose')//mongoose(mongodb를 쉽게 이용할 수 있는 tool)를 이용해 connect함
-console.log(config)
-console.log(config.mongoURI)
 mongoose.connect(config.mongoURI)
 .then(()=>console.log('Mongodb connected...'))
 .catch(err=>console.log(err))
@@ -34,7 +33,7 @@ mongoose.connect(config.mongoURI)
 app.get('/', (req, res)=>res.send('Hello world hihihi')) //현재 디렉토리에서 hello world 출력하기
 
 
-
+// <1> register route
 app.post('/register', (req, res)=>{//post 메소드, route의 엔드포인트는 /register, callback function의 인자로 req, res
  //회원가입할 때 필요한 정보들을 client에서 가져오면
  //그것을 database에 넣어준다.
@@ -42,12 +41,47 @@ app.post('/register', (req, res)=>{//post 메소드, route의 엔드포인트는
     //req.body에 User정보가 들어 있음
     //bodyparser가 정보를 받아줌
     //ex User {name: juurom, email: bbjjyy01@gmail.com, ...}
+    
+
+    //save 하기전 비밀번호 암호화
     user.save((err, userInfo)=>{
         if (err) return res.json({success: false, err})
         return res.status(200).json({success: true})
     }) //mongodb 메소드
         //error 실패 정보 전달, 성공 시 res.status(200)로 성공 정보 전달
 
+})
+
+//<2> login route
+app.post('/login', (req, res)=>{
+    //(1)요청된 이메일을 데이터베이스에서 찾는다
+    //findOne: mongodb에서 제공하는 탐색 메소드
+    User.findOne({email: req.body.email}, (err, userinfo)=>{
+        if(!userinfo) {return res.json({
+            loginSuccess: false,
+            message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+
+        //(2)이메일이 있다면 비밀번호가 같은지 확인한다
+        userinfo.comparePassword(req.body.password, (err, isMatch) =>{
+            if (!isMatch){
+                return res.json({loginSuccess:false, message:"비밀번호가 틀렸습니다."})
+                }
+            else{
+                userinfo.generateToken((err, user)=>{
+                if(err) return res.status(400).send(err);
+                //토큰을 저장해야 함. 쿠키, 로컬스토리지, ...
+                //여러 가지 방법이 있지만 여기서는 쿠키에 저장함
+                res.cookie("x_auth", user.token)
+                //개발자도구-application-cookie에 저장됨
+                .status(200)
+                .json({loginSuccess: true, userId: user._id})
+                })
+            }
+        })
+    })
+    //(3)비밀번호가 맞다면 유저를 위한 토큰을 생성한다
 })
 
 app.listen(port, () => console.log(`example app listning on port ${port}!`)); //현재 앱이 port에서 실행되고 있음
--- models/User.js
+++ models/User.js
@@ -1,4 +1,16 @@
+//왜 자꾸 뜨는지는 잘 모르겠지만
+//Error: listen EADDRINUSE: address already in use :::5000
+//이 에러가 뜨면 이미 5000port가 실행중이라 또 실행시킬 수 없다는 뜻이다
+//해결방법은 windows cmd창에서 프로세스를 강제종료시켜주면 된다.
+//(1) netstat -ano | findstr :5000
+//>>> TCP (~~~~) LISTENING (*PID num*)
+//(2) taskkill /PID (*PID num*) /F
+//>>> 성공: 프로세스가 종료되었습니다.
+
 const mongoose = require('mongoose') //mongoose 모듈 가져오기
+const bcrypt = require('bcrypt') //bcrypt 모듈 가져오기
+const saltRounds = 10; //salt의 자릿수. 암호화할 때 사용됨
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({ //mongoose로 스키마 만들기
     name:{
@@ -11,7 +23,7 @@ const userSchema = mongoose.Schema({ //mongoose로 스키마 만들기
     },
     password:{
         type: String,
-        maxlength: 50
+        maxlength: 200
     },
     role:{
         type: Number,
@@ -25,6 +37,51 @@ const userSchema = mongoose.Schema({ //mongoose로 스키마 만들기
         type: Number,
     }
 })
+
+//pre: mongoose 메소드, .save하기 전에 function을 하고, next()로 .save를 한다~
+userSchema.pre('save', function(next){
+    //(1) 비밀번호 암호화
+    //아래 코드는 bcrypt 사이트 - usage에 있음
+    //https://www.npmjs.com/package/bcrypt
+    let user = this; //this는 위에 있는 userSchema를 가리킴
+    if (user.isModified('password')){ //password가 수정될 때만 암호화하기
+        const myPlaintextPassword = user.password;
+        bcrypt.genSalt(saltRounds, function(err, salt) { //saltRound의 자릿수만큼 salt를 만듦
+            if (err) return next(err)//에러가 나면 next 단계에 error를 전달해줌
+            bcrypt.hash(myPlaintextPassword, salt, function(err, hash){
+                if(err) return next(err);
+                user.password = hash; //hash된 비밀번호로 바꿔주기
+                next();//완성됐으니 .save로 ㄱㄱ
+            })
+            //myPlaintextPassword: 원래비밀번호. 000123
+        });
+    }else{//이런... 여기서 else를 안해주면 요 아래 나오는 next가 먼저 실행돼서 hashing이 되지 않았던 것이다
+        //(2) .save
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    //plainpassword와 암호화된 비밀번호가 일치하는지 확인
+    //plainPassword를 암호화한 뒤, db에 있는 비밀번호와 일치하는지 확인
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err);
+        cb(null, isMatch)//isMatch=true
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+    //jsonwebtoken을 이용해 token을 생성한다
+    let user=this;
+    let token=jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token=token;
+    user.save(function(err, user){
+        if (err) return cb(err);
+        cb(null, user)
+    })
+    //user._id + 'secretToken' = token
+    //token을 통해 user._id를 찾아낼 수 있음
+}
 
 const User = mongoose.model('User', userSchema) //모델에 만든 스키마 넣기
 
